321319bbede6866f81811b25ee7b71d9248f3a73
Emtiaz-pias <emtiazpias383@gmail.com>
2023-07-28 00:31:01 +0600

    expiry query api done

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ async function run() {
     const database = client.db(db);
     const outletCollection = database.collection(process.env.OUTLETS_COLLECTION);
     const productsCollection = database.collection(process.env.PRODUCTS_COLLECTION);
-    const expiryCollection =database.collection(process.env.EXPIRY_COLLECTION);
-    const deliveryCollection =database.collection(process.env.DELIVERY_COLLECTION);
+    const expiryCollection = database.collection(process.env.EXPIRY_COLLECTION);
+    const deliveryCollection = database.collection(process.env.DELIVERY_COLLECTION);
 
 
 
@@ -52,21 +52,21 @@ async function run() {
       try {
         const data = req.body;
         const upload = await productsCollection.insertOne(data);
-        console.log(req.body,upload)
+        console.log(req.body, upload)
         res.send(upload.acknowledged)
       } catch (err) {
         console.error("Error inserting data:", err);
         res.status(500).json({ error: "An error occurred while inserting data." });
       }
     });
-    app.post('/postNewExpriry',async function (req, res) {
+    app.post('/postNewExpriry', async function (req, res) {
       const expiry = req.body
       const upload = await expiryCollection.insertOne(expiry)
       res.send(upload.acknowledged)
 
     })
 
-    app.post('/postNewDelivery',async function (req, res) {
+    app.post('/postNewDelivery', async function (req, res) {
       const delivery = req.body
       const upload = await deliveryCollection.insertOne(delivery)
       res.send(upload.acknowledged)
@@ -76,15 +76,35 @@ async function run() {
     //////get APIs/////////////////
 
     //get all outlets//
-    app.get('/getAllOutlets',async (req,res)=>{
+    app.get('/getAllOutlets', async (req, res) => {
       const getOutlets = await outletCollection.find({}).toArray()
       res.send(getOutlets)
     })
     // get all products///
-    app.get('/getAllProducts',async (req,res)=>{
+    app.get('/getAllProducts', async (req, res) => {
       const getProducts = await productsCollection.find({}).toArray()
       res.send(getProducts)
     })
+
+
+    // find api's
+
+    // find expriy according to outlet and date..
+    app.get('/getExpiry', async (req, res) => {
+
+      const { outlet, dateFrom, dateTo } = req.query;
+      //expiryCollection
+      const query = {
+        outletName: outlet, expiryDate: {
+          $gte: dateFrom,
+          $lte:dateTo,
+        },
+      }
+      const expiryData = await expiryCollection.find(query).toArray();
+      res.send(expiryData)
+    })
+
+
     const PORT = process.env.PORT || 4040;
     app.listen(PORT, () => {
       console.log("app is alive N connected to db ;) on port =>", PORT);
